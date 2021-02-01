d3de395e8ef5654bf53873e5e3aa1f245a3ca422
crystal-csete <crystal-csete@lambdastudents.com>
2021-02-01 08:17:52 -0700

    added

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,3 @@
-// const pg = require('pg')
-
-// const localConnection = 'postgresql://localhost/auth'
-
-// let connection
-
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false }
-//   connection = process.env.DATABASE_URL
-// } else {
-//   connection = localConnection
-// }
-
-// const sharedConfig = {
-//   client: 'pg',
-//   connection,
-//   migrations: { directory: './database/migrations' },
-//   seeds: { directory: './database/seeds' },
-// }
-
-// module.exports = {
-//   development: { ...sharedConfig },
-//   production: {
-//     ...sharedConfig,
-//     pool: { min: 2, max: 10 },
-//   },
-// }
-
-
-
 
 require("dotenv").config();
 
