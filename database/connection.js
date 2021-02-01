85a0f5b53b49ead134da6103d39e7027a3c5c6f8
crystal-csete <crystal-csete@lambdastudents.com>
2021-01-31 20:07:19 -0700

    added config

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,8 @@
-const knex = require("knex");
+require("dotenv").config();
+
+const knex = require("knex")("production");
 
 const knexfile = require("../knexfile.js");
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "production";
 
 module.exports = knex(knexfile[environment]);
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+require("dotenv").config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => console.log(`\n** Server is running on http://localhost:${port} **\n`));
